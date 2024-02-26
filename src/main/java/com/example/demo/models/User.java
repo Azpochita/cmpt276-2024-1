f4d8544d78016af90a26be4e7b01f6d2143e6f66
Aaron Zhou <aaa.aaronzhou@gmail.com>
2024-02-25 19:30:13 -0800

    commit

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,53 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+    private String name;
+    private String password;
+    private int age;
+
+    public User(String name, String password, int age) {
+        this.name = name;
+        this.password = password;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    
+}
