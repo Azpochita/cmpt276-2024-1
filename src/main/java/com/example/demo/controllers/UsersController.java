b1e559065a9f780a02e829cb2781d4a2d3d9d7a8
Aaron Zhou <aaa.aaronzhou@gmail.com>
2024-02-25 21:32:17 -0800

    change endpoint to student

---
+++ src/main/java/com/example/demo/controllers/UsersController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.example.demo.models.UserRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.*;
+
+import com.example.demo.models.User;
+
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@Controller
+public class UsersController {
+    
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("/users/view")
+    public String getAllUsers(Model model) {
+        System.out.println("Hello from all users");
+        List<User> users = userRepo.findAll(); // db
+        model.addAttribute("us", users);
+        return "users/showAll";
+    }
+    
+    @PostMapping("/student/add")
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
+        System.out.println("ADD user");
+        String newName = newuser.get("name");
+        String newPwd = newuser.get("password");
+        int newSize = Integer.parseInt(newuser.get("size"));
+        userRepo.save(new User(newName,newPwd,newSize));
+        response.setStatus(201);
+        return "users/addedUser";
+    }
+    // @GetMapping("/login")
+    // public String getLogin(Model model, HttpServletRequest request, HttpSession session) {
+    //     return new String();
+    // }
+    
+}
