18e13fa4186beae32afba94dcc8866e3ee07d881
Aaron Zhou <aaa.aaronzhou@gmail.com>
2024-02-25 22:24:27 -0800

    Update StudentRepository.java

--- src/main/java/com/example/demo/models/StudentRepository.java
+++ src/main/java/com/example/demo/models/StudentRepository.java
@@ -1,10 +1,11 @@
 package com.example.demo.models;
 
 import java.util.List;
+import com.example.models.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Integer>{
+public interface StudentRepository extends JpaRepository<Student, Integer> {
     List<Student> findByAge(int age);
     List<Student> findByName(String name);
     List<Student> findByNameAndPassword(String name, String password);
